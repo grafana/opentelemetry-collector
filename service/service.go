5cff329437234b321982e1c43eb36b8439f3ab5e
Paulin Todev <paulin.todev@gmail.com>
2023-06-30 19:57:29 +0100

    Add an option to disable process metrics

--- service/service.go
+++ service/service.go
@@ -65,6 +65,8 @@ type Settings struct {
 
 	UseExternalMetricsServer bool
 
+	DisableProcessMetrics bool
+
 	TracerProvider trace.TracerProvider
 
 	// For testing purpose only.
@@ -218,7 +220,7 @@ func (srv *Service) initExtensionsAndPipeline(ctx context.Context, set Settings,
 		return fmt.Errorf("failed to build pipelines: %w", err)
 	}
 
-	if cfg.Telemetry.Metrics.Level != configtelemetry.LevelNone && cfg.Telemetry.Metrics.Address != "" {
+	if !set.DisableProcessMetrics && cfg.Telemetry.Metrics.Level != configtelemetry.LevelNone && (set.UseExternalMetricsServer || cfg.Telemetry.Metrics.Address != "") {
 		// The process telemetry initialization requires the ballast size, which is available after the extensions are initialized.
 		if err = proctelemetry.RegisterProcessMetrics(srv.telemetryInitializer.ocRegistry, srv.telemetryInitializer.mp, obsreportconfig.UseOtelForInternalMetricsfeatureGate.IsEnabled(), getBallastSize(srv.host)); err != nil {
 			return fmt.Errorf("failed to register process metrics: %w", err)
