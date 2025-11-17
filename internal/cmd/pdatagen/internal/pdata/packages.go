589c5abdfc3f2a67a2c22e4f50fb31e67a88a2aa
Laurent Dufresne <laurent.dufresne@grafana.com>
2025-11-17 16:56:02 +0100

    Added lazy annotation

--- internal/cmd/pdatagen/internal/pdata/packages.go
+++ internal/cmd/pdatagen/internal/pdata/packages.go
@@ -13,6 +13,7 @@ import (
 )
 
 var nonInternalDeps = []string{
+	`"go.opentelemetry.io/collector/pdata"`,
 	`"go.opentelemetry.io/collector/pdata/internal"`,
 	`"go.opentelemetry.io/collector/pdata/pcommon"`,
 	`"go.opentelemetry.io/collector/pdata/plog"`,
@@ -112,6 +113,10 @@ func (p *Package) GenerateProtoMessageFiles() error {
 		})
 		path := filepath.Join("pdata", "internal", "generated_proto_"+strings.ToLower(s.getOriginName())+".go")
 		if err := os.WriteFile(path, pm.GenerateMessage(p.info.imports, p.info.testImports), 0o600); err != nil {
+			return err
+		}
+		path = filepath.Join("pdata", "internal", "generated_proto_lazy"+strings.ToLower(s.getOriginName())+".go")
+		if err := os.WriteFile(path, pm.GenerateLazyMessage(p.info.imports, p.info.testImports), 0o600); err != nil {
 			return err
 		}
 		p.info.testImports = saveTestImports
